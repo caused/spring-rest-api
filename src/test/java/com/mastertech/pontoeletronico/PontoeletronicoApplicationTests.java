c2eaab944c42ab7c5a0a3cb69dbe75d55661cdc2
gaswkxa <gustavo.a.santos-silva@itau-unibanco.com.br>
2020-03-02 13:46:20 -0300

    feat: criando rotas para persistencia de usuario

---
+++ src/test/java/com/mastertech/pontoeletronico/PontoeletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastertech.pontoeletronico;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoeletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
